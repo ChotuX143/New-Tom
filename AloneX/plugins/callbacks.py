2ae1efee5af96606e988a32acd2887296d938193
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2026-06-01 08:45:44 +0530

    𝐂ᴏᴍᴍᴀɴᴅ 𝐅ɪx 𝐁ʏ 𝐀ʟᴏɴᴇ 𝐂ᴏᴅᴇʀ

--- AloneX/plugins/callbacks.py
+++ AloneX/plugins/callbacks.py
@@ -125,7 +125,8 @@ async def _help(_, query: types.CallbackQuery):
         try:
             return await query.edit_message_text(
                 text=query.lang["help_menu"],
-                reply_markup=buttons.help_markup(query.lang),
+               # reply_markup=buttons.help_markup(query.lang),
+                reply_markup=buttons.settings_back_helper_markup(query.lang),
             )
         except:
             return await query.edit_message_caption(

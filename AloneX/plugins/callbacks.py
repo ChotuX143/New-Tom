e7de3205bea4ff3041ddfe6eaaf739c31d6f354e
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2026-06-01 08:40:32 +0530

    𝐂ᴏᴍᴍᴀɴᴅ 𝐅ɪx 𝐁ʏ 𝐀ʟᴏɴᴇ 𝐂ᴏᴅᴇʀ

--- AloneX/plugins/callbacks.py
+++ AloneX/plugins/callbacks.py
@@ -116,7 +116,8 @@ async def _controls(_, query: types.CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("^(help|settings_back_helper)") & ~app.bl_users)
+#@app.on_callback_query(filters.regex("^(help|settings_back_helper)") & ~app.bl_users)
+@app.on_callback_query(filters.regex("^settings_back_helper") & ~app.bl_users)
 @lang.language()
 async def _help(_, query: types.CallbackQuery):
     data = query.data.split()
